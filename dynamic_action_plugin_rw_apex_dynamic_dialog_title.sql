33b17305f419d452a958f74a56c07150be8d124f
Ronny Weiss <30749177+RonnyWeiss@users.noreply.github.com>
2023-01-12 21:40:42 +0100

    fix that only currently opened dialog title is changed

--- dynamic_action_plugin_rw_apex_dynamic_dialog_title.sql
+++ dynamic_action_plugin_rw_apex_dynamic_dialog_title.sql
@@ -6,7 +6,7 @@ whenever sqlerror exit sql.sqlcode rollback
 -- ORACLE Application Express (APEX) export file
 --
 -- You should run the script connected to SQL*Plus as the Oracle user
--- APEX_200100 or as the owner (parsing schema) of the application.
+-- APEX_210200 or as the owner (parsing schema) of the application.
 --
 -- NOTE: Calls to apex_application_install override the defaults below.
 --
@@ -15,9 +15,10 @@ begin
 wwv_flow_api.import_begin (
  p_version_yyyy_mm_dd=>'2016.08.24'
 ,p_release=>'5.1.3.00.05'
-,p_default_workspace_id=>21717127411908241868
-,p_default_application_id=>103428
-,p_default_owner=>'RD_DEV'
+,p_default_workspace_id=>100000
+,p_default_application_id=>55310
+,p_default_id_offset=>22598797485307386842
+,p_default_owner=>'RONNY'
 );
 end;
 /
@@ -30,7 +31,7 @@ end;
 prompt --application/shared_components/plugins/dynamic_action/rw_apex_dynamic_dialog_title
 begin
 wwv_flow_api.create_plugin(
- p_id=>wwv_flow_api.id(36102813978279109574)
+ p_id=>wwv_flow_api.id(45227022029372902468)
 ,p_plugin_type=>'DYNAMIC ACTION'
 ,p_name=>'RW.APEX.DYNAMIC.DIALOG.TITLE'
 ,p_display_name=>'APEX Dynamic Dialog Title'
@@ -46,7 +47,7 @@ wwv_flow_api.create_plugin(
 '    VR_FUNCTION_RESULT   P_DYNAMIC_ACTION.ATTRIBUTE_01%TYPE := APEX_PLUGIN_UTIL.GET_PLSQL_FUNCTION_RESULT(P_PLSQL_FUNCTION => P_DYNAMIC_ACTION.ATTRIBUTE_01);',
 'BEGIN',
 '',
-'    VR_RESULT.JAVASCRIPT_FUNCTION := ''function () { apex.util.getTopApex().jQuery(".ui-dialog-content").dialog("option", "title", '' ||',
+'    VR_RESULT.JAVASCRIPT_FUNCTION := ''function () { apex.util.getTopApex().jQuery(".ui-dialog-content").last().dialog("option", "title", '' ||',
 '    APEX_JAVASCRIPT.ADD_VALUE(',
 '        P_VALUE       => VR_FUNCTION_RESULT,',
 '        P_ADD_COMMA   => FALSE',
@@ -60,13 +61,13 @@ wwv_flow_api.create_plugin(
 ,p_substitute_attributes=>true
 ,p_subscribe_plugin_settings=>true
 ,p_help_text=>'This plug-in is used to set a dynamic modal dialog title on dialog render. Just add this Plug-in to a Dynamic Action that is fired on Page Load.'
-,p_version_identifier=>'1.0'
+,p_version_identifier=>'1.1'
 ,p_about_url=>'https://github.com/RonnyWeiss/APEX-Dynamic-Dialog-Title'
 ,p_files_version=>10
 );
 wwv_flow_api.create_plugin_attribute(
- p_id=>wwv_flow_api.id(36104646835276167628)
-,p_plugin_id=>wwv_flow_api.id(36102813978279109574)
+ p_id=>wwv_flow_api.id(45228854886369960522)
+,p_plugin_id=>wwv_flow_api.id(45227022029372902468)
 ,p_attribute_scope=>'COMPONENT'
 ,p_attribute_sequence=>1
 ,p_display_sequence=>10
@@ -115,8 +116,8 @@ end;
 /
 begin
 wwv_flow_api.create_plugin_file(
- p_id=>wwv_flow_api.id(36102822093389109606)
-,p_plugin_id=>wwv_flow_api.id(36102813978279109574)
+ p_id=>wwv_flow_api.id(45227030144482902500)
+,p_plugin_id=>wwv_flow_api.id(45227022029372902468)
 ,p_file_name=>'LICENSE'
 ,p_mime_type=>'application/octet-stream'
 ,p_file_charset=>'utf-8'
